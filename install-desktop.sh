be58b4163ea2402e78f69618f23d14d79b6507e6
Stefan Partheymüller <s.partheymueller@initics.de>
2025-07-06 22:36:15 +0200

    feat(hyprland): first usable state

--- install-desktop.sh
+++ install-desktop.sh
@@ -6,13 +6,38 @@
 
 source ./utils.sh
 
-info "Installing hyprland and tools"
-$PKG_INSTALL hyprland \
-  hyprpaper \
-  hyprlock \
-  wofi \
-  waybar \
-  brightnessctl \
-  playerctl
+info "Installing display manager and wayland session manager"
+$PKG_INSTALL ly uwsm
 result
 
+info "Installing window manager and tools"
+$PKG_INSTALL \
+  hyprland \
+  hyprsunset hyprpicker hyprshot hyprpaper hyprlock hypridle hyprpolkitagent hyprland-qtutils \
+  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk qt6-wayland qt5-wayland \
+  wofi \
+  waybar \
+  swaync \
+  pavucontrol brightnessctl playerctl \
+  pacman-contrib
+result
+
+info "Installing clipboard manager"
+$PKG_INSTALL wl-clipboard clipse
+result
+
+info "Installing theming packages"
+$PKG_INSTALL gnome-themes-extra kvantum-qt5 nwg-look
+result
+
+info "Installing terminal emulator"
+$PKG_INSTALL ghostty
+result
+
+info "Installing differ"
+$PKG_INSTALL meld
+result
+
+info "Installing file manager"
+$PKG_INSTALL nautilus
+result
