907059909fa8d1aa02fdba7cb72e6d201dba53a3
Stefan Partheymüller <s.partheymueller@initics.de>
2025-07-08 23:09:07 +0200

    feat(desktop): add nautilus and blueberry to installer

--- install-desktop.sh
+++ install-desktop.sh
@@ -23,16 +23,24 @@ $PKG_INSTALL \
   pacman-contrib
 result
 
-info "Installing clipboard manager"
-$PKG_INSTALL wl-clipboard clipse
-result
-
 info "Installing theming packages"
 $PKG_INSTALL gnome-themes-extra kvantum-qt5 nwg-look
 result
 
+info "Installing clipboard manager"
+$PKG_INSTALL wl-clipboard clipse
+result
+
 info "Installing terminal emulator"
 $PKG_INSTALL ghostty
+result
+
+info "Installing file manager"
+$PKG_INSTALL nautilus
+result
+
+info "Installing bluetooth manager"
+$PKG_INSTALL blueberry
 result
 
 info "Installing differ"
